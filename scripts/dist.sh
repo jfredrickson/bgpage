71db883c9eb465a8fe4231799ecc717d909c3ef2
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2019-10-16 17:06:39 -0600

    clean up unused code

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
-# Build a distribution zip file.
-
-# Increment build number
-#versionNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" ${INFOPLIST_FILE})
-#buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" ${INFOPLIST_FILE})
-#buildNumber=$((${buildNumber} + 1))
-#/usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${buildNumber}" ${INFOPLIST_FILE}
-#echo Packaging version ${versionNumber} build ${buildNumber}
+#!/bin/sh
 
 # Create zip package
 distPath=${PROJECT_DIR}/dist
@@ -14,10 +6,3 @@ distFile=${distPath}/bgpage.zip
 mkdir -p ${distPath}
 rm -f ${distFile}
 zip ${distFile} -j ${BUILT_PRODUCTS_DIR}/bgpage ${PROJECT_DIR}/agent/bgpage.plist
-
-# Group distributable files together
-#distPath=${PROJECT_DIR}/dist
-#mkdir -p ${distPath}
-#rm -f ${distPath}/*
-#cp ${BUILT_PRODUCTS_DIR}/bgpage ${distPath}
-#cp ${PROJECT_DIR}/agent/bgpage.plist ${distPath}
