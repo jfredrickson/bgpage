e30b690be5aaa9dcf392609bbd95e8db2c44a7ce
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2019-10-16 16:00:05 -0600

    copy files to dist without zipping; eliminate local versioning

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -2,15 +2,22 @@
 # Build a distribution zip file.
 
 # Increment build number
-versionNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" ${INFOPLIST_FILE})
-buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" ${INFOPLIST_FILE})
-buildNumber=$((${buildNumber} + 1))
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${buildNumber}" ${INFOPLIST_FILE}
-echo Packaging version ${versionNumber} build ${buildNumber}
+#versionNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" ${INFOPLIST_FILE})
+#buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" ${INFOPLIST_FILE})
+#buildNumber=$((${buildNumber} + 1))
+#/usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${buildNumber}" ${INFOPLIST_FILE}
+#echo Packaging version ${versionNumber} build ${buildNumber}
 
 # Create zip package
+#distPath=${PROJECT_DIR}/dist
+#distFile=${distPath}/bgpage-${versionNumber}.zip
+#mkdir -p ${distPath}
+#rm -f ${distFile}
+#zip ${distFile} -j ${BUILT_PRODUCTS_DIR}/bgpage ${PROJECT_DIR}/agent/bgpage.plist
+
+# Group distributable files together
 distPath=${PROJECT_DIR}/dist
-distFile=${distPath}/bgpage-${versionNumber}.zip
 mkdir -p ${distPath}
-rm -f ${distFile}
-zip ${distFile} -j ${BUILT_PRODUCTS_DIR}/bgpage ${PROJECT_DIR}/agent/bgpage.plist
+rm -f ${distPath}/*
+cp ${BUILT_PRODUCTS_DIR}/bgpage ${distPath}
+cp ${PROJECT_DIR}/agent/bgpage.plist ${distPath}
