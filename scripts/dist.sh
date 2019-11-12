61ed8d819a775f802cf42526fb2d7e9ea2c0ce9b
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2019-11-12 13:48:22 -0700

    improved zip file structure

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
+#
 # Build a distributable package
+#
 
 # Use the most recent git tag as a version
 latestTag=$(git tag -l | tail -1)
 
-# Create zip package
+# Set up for zip package
 distPath=${PROJECT_DIR}/dist
 distFile=${distPath}/bgpage-${latestTag}.zip
 mkdir -p ${distPath}
 rm -f ${distFile}
-zip ${distFile} -j ${BUILT_PRODUCTS_DIR}/bgpage ${PROJECT_DIR}/agent/bgpage.plist
+
+# Add executable
+pushd ${BUILT_PRODUCTS_DIR}
+  zip -r ${distFile} bgpage
+popd
+
+# Add examples
+pushd ${PROJECT_DIR}
+  zip -r ${distFile} examples
+popd
