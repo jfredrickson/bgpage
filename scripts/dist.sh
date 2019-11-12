64fc75960cd52f09961701f20354c827e9f6f104
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2019-11-12 13:20:42 -0700

    add version to zip file name

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+# Build a distributable package
+
+# Use the most recent git tag as a version
+latestTag=$(git tag -l | tail -1)
 
 # Create zip package
 distPath=${PROJECT_DIR}/dist
-distFile=${distPath}/bgpage.zip
+distFile=${distPath}/bgpage-${latestTag}.zip
 mkdir -p ${distPath}
 rm -f ${distFile}
 zip ${distFile} -j ${BUILT_PRODUCTS_DIR}/bgpage ${PROJECT_DIR}/agent/bgpage.plist
